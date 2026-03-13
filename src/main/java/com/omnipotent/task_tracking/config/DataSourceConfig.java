45e7c9d6ca6bdb46520bc54695dd41898afc00c3
Ralph Aubeng <rgradison@gmail.com>
2026-03-13 16:14:18 +0200

    added DatabaseProperties, DataSourceConfig

--- src/main/java/com/omnipotent/task_tracking/config/DataSourceConfig.java
+++ src/main/java/com/omnipotent/task_tracking/config/DataSourceConfig.java
@@ -1,24 +1,4 @@
 package com.omnipotent.task_tracking.config;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
-@Configuration
 public class DataSourceConfig {
-    @Bean
-    public DataSource dataSource(DatabaseProperties props) {
-
-        HikariDataSource ds = new HikariDataSource();
-        ds.setJdbcUrl(props.url());
-        ds.setUsername(props.username());
-        ds.setPassword(props.password());
-        ds.setDriverClassName(props.driverClassName());
-
-        ds.setMaximumPoolSize(props.hikari().maximumPoolSize());
-
-        return ds;
-    }
 }
--- src/main/java/com/omnipotent/task_tracking/config/DatabaseProperties.java
+++ src/main/java/com/omnipotent/task_tracking/config/DatabaseProperties.java
@@ -1,16 +1,88 @@
 package com.omnipotent.task_tracking.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
-@ConfigurationProperties(prefix = "spring.datasource")
-public record DatabaseProperties(
-        String url,
-        String username,
-        String password,
-        String driverClassName,
-        Hikari hikari
-) {
-    public record Hikari(
-            int maximumPoolSize
-    ) {}
+import javax.sql.DataSource;
+
+@Configuration
+@EnableConfigurationProperties(DataSourceConfig.ValidatedDataSourceProperties.class)
+public class DataSourceConfig {
+    /*
+       Advantages
+       Type-safe
+       Supports nested configs
+       Supports validation
+       Clean for large configs
+     */
+    @Validated
+    @ConfigurationProperties("spring.datasource")
+    public static class ValidatedDataSourceProperties {
+        @NotBlank private String url;
+        @NotBlank private String username;
+        @NotBlank private String password;
+        @NotBlank private String driverClassName;
+
+        private Hikari hikari = new Hikari();
+
+        public static class Hikari {
+            @Min(1)
+            private int maximumPoolSize = 10; // default
+
+            @Min(0)
+            private int minimumIdle = 5;      // default
+            @Min(0)
+            private long idleTimeout = 600_000; // default in ms
+            @Min(0)
+            private long maxLifetime = 1_800_000; // default in ms
+            @Min(0)
+            private long leakDetectionThreshold = 2_000; // default in ms
+
+            // Getters and setters
+            public int getMaximumPoolSize() { return maximumPoolSize; }
+            public void setMaximumPoolSize(int maximumPoolSize) { this.maximumPoolSize = maximumPoolSize; }
+
+            public int getMinimumIdle() { return minimumIdle; }
+            public void setMinimumIdle(int minimumIdle) { this.minimumIdle = minimumIdle; }
+
+            public long getIdleTimeout() { return idleTimeout; }
+            public void setIdleTimeout(long idleTimeout) { this.idleTimeout = idleTimeout; }
+
+            public long getMaxLifetime() { return maxLifetime; }
+            public void setMaxLifetime(long maxLifetime) { this.maxLifetime = maxLifetime; }
+
+            public long getLeakDetectionThreshold() { return leakDetectionThreshold; }
+            public void setLeakDetectionThreshold(long leakDetectionThreshold) { this.leakDetectionThreshold = leakDetectionThreshold; }
+        }
+
+        // getters & setters
+        public String getUrl() { return url; }
+        public void setUrl(String url) { this.url = url; }
+        public String getUsername() { return username; }
+        public void setUsername(String username) { this.username = username; }
+        public String getPassword() { return password; }
+        public void setPassword(String password) { this.password = password; }
+        public String getDriverClassName() { return driverClassName; }
+        public void setDriverClassName(String driverClassName) { this.driverClassName = driverClassName; }
+        public Hikari getHikari() { return hikari; }
+        public void setHikari(Hikari hikari) { this.hikari = hikari; }
+    }
+
+    // Spring will inject the auto-created bean
+    @Bean
+    public DataSource dataSource(ValidatedDataSourceProperties props) {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(props.getUrl());
+        ds.setUsername(props.getUsername());
+        ds.setPassword(props.getPassword());
+        ds.setDriverClassName(props.getDriverClassName());
+        ds.setMaximumPoolSize(props.getHikari().getMaximumPoolSize());
+        return ds;
+    }
 }
