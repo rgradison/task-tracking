6e915d5d1633c7f93982f2deb4ca3776009800e2
Ralph Aubeng <rgradison@gmail.com>
2026-03-22 13:56:16 +0200

    database properties war configs

--- src/test/java/com/omnipotent/task_tracking/TaskTrackingApplicationTests.java
+++ src/test/java/com/omnipotent/task_tracking/TaskTrackingApplicationTests.java
@@ -2,8 +2,10 @@ package com.omnipotent.task_tracking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TaskTrackingApplicationTests {
 
 	// Starts the full Spring context
